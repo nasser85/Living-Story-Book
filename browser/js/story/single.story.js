162e74f927138b1fecbd25760c76fd1bea9d824a
Nasser Abouelazm <nasser.abouelazm@gmail.com>
2016-09-17 23:51:00 -0400

    voice features added as well as improved image selection

--- browser/js/story/single.story.js
+++ browser/js/story/single.story.js
@@ -19,4 +19,26 @@ app.controller('SingleStoryCtrl', function($scope, StoryFactory, story, author) 
     $scope.newStory = story;
     $scope.pages = story.pages;
     console.log('here is the single story: ', story);
+    var voice = null;
+    $scope.readAloud = function(text) {
+        voice = null;
+        
+        var msg = new SpeechSynthesisUtterance(text);
+        var voices = window.speechSynthesis.getVoices();
+        console.log(voices);
+        msg.voice = voices.filter(function(el) {
+            return el.name === 'Cellos';
+        })[0];
+        console.log(msg);
+        window.speechSynthesis.speak(msg);
+        // voice = VoiceRSS.speech({
+        //     key: '2e714518e6ba46dd9c4872900e88255c',
+        //     src: text,
+        //     hl: 'en-gb',
+        //     r: 0, 
+        //     c: 'mp3',
+        //     f: '44khz_16bit_stereo',
+        //     ssml: false
+        // });
+    }
 });
